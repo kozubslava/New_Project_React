438649048069e81f5cebc3d2c13546a6e2c9ee4b
Вячеслав <kozub.vm@gmail.com>
2024-02-05 22:20:17 +0100

    add handleCoords

--- src/components/HardState/index.jsx
+++ src/components/HardState/index.jsx
@@ -8,16 +8,15 @@ const initialState = {
   },
 };
 
-// функція яка поверне нове значення стану, яке реакт збереже і викличе ререндер
 function reducer(state, action) {
-  console.log(state, action);
-  if ((action.type = "number")) {
+  if (action.type === "number") {
+    console.log(action.type, action.type === "number");
     const newState = {
       ...state,
       number: action.payload,
     };
     return newState;
-  } else if ((action.type = "coords")) {
+  } else if (action.type === "coords") {
     const newState = {
       ...state,
       coords: action.newCoords,
@@ -29,28 +28,13 @@ function reducer(state, action) {
 }
 
 const HardState = (props) => {
-  // state - стан
-  // dispatch - функція відправки
-  // reducer - функція, яка оновлюе стан, приймає найсвіжіший state першим аргументом
-  // і action (набір додаткових даних для зміни стану) другим аргументом
-  // reducer має повертати новий стан на основі цих даних
-  // initialState - початкове значення стану
   const [state, dispatch] = useReducer(reducer, initialState);
   const { coords, number } = state;
 
-  // їх тут 5+
-  // const [coords, setCoords] = useState({ x: 0, y: 0 });
-  // const [number, setNumber] = useState(1);
-
-  // тут теж 5+
   function handleChange({ target: { value } }) {
-    // дані для ред'юсеру які дозволяють зрозуміти яку логіку стану використовувати
     const action = { type: "number", payload: +value };
 
-    // відправка action каже реакту запускати ред'юсер
     dispatch(action);
-
-    // setNumber(+value);
   }
 
   function handleCoords({ clientX, clientY }) {
